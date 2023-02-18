5aa8a542b7a97f6ae970e9c241df03867a28183e
moinulhuq <moinulhuq@gmail.com>
2023-02-19 04:51:19 +1300

    hello world maven project

---
+++ src/main/java/com/example/helloworld/HelloworldApplication.java
@@ -0,0 +1,30 @@
+package com.example.helloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//--------------- Extra --------------
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableAutoConfiguration
+//--------------- end --------------
+
+@SpringBootApplication
+public class HelloworldApplication {
+
+//--------------- Extra --------------
+    @RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
+//--------------- end --------------
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloworldApplication.class, args);
+	}
+
+}
